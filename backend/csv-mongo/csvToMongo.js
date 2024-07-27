c67f412aa6034ba7b6b2bc04c83f4442781545d4
AitijhyaModak <aitijhyasps1@gmail.com>
2024-07-27 18:16:29 +0530

    modified csvToMongo.js

--- backend/csv-mongo/csvToMongo.js
+++ backend/csv-mongo/csvToMongo.js
@@ -6,25 +6,29 @@ import { Students } from "../models/students";
 
 const results = [];
 
-fs.createReadStream("mockdata.csv")
-  .pipe(csv())
-  .on("data", (data) => results.push(data))
-  .on("end", () => {
-    const formattedResults = results.map((item) => ({
-      firstName: item["First Name"],
-      lastName: item["Last Name"],
-      age: parseInt(item["Age"], 10),
-      class: item["Class"],
-      annualIncome: parseInt(item["Annual Income"], 10),
-    }));
+async function csvToMongo() {
+  fs.createReadStream("mockdata.csv")
+    .pipe(csv())
+    .on("data", (data) => results.push(data))
+    .on("end", () => {
+      const formattedResults = results.map((item) => ({
+        firstName: item["First Name"],
+        lastName: item["Last Name"],
+        age: parseInt(item["Age"], 10),
+        class: item["Class"],
+        annualIncome: parseInt(item["Annual Income"], 10),
+      }));
 
-    Students.insertMany(formattedResults)
-      .then((docs) => {
-        console.log("Data inserted successfully:", docs);
-        mongoose.connection.close();
-      })
-      .catch((err) => {
-        console.error("Error inserting data:", err);
-        mongoose.connection.close();
-      });
-  });
+      Students.insertMany(formattedResults)
+        .then((docs) => {
+          console.log("Data inserted successfully:", docs);
+          mongoose.connection.close();
+        })
+        .catch((err) => {
+          console.error("Error inserting data:", err);
+          mongoose.connection.close();
+        });
+    });
+}
+
+export default csvToMongo;
