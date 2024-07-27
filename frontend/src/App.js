647a7cfc6ae9faa992c921bae486bd00254abd61
nimit bajaj <student@nimit.bajaj2005@gmail.com>
2024-07-28 04:13:02 +0530

    Donator dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './components/RegistrationForm/SignIn/SignIn';
 import Registrationform from './components/RegistrationForm/Registration/registrationform';
+import EduParentDashboard from './components/RegistrationForm/EduParent/EduParentDashboard';
 
 const App = () => {
   return (
-    <div className="App">
-        <div className='App-logo'>
-        <Router>
-      <Routes>
-        <Route path="/register" element={<Registrationform />} />
-        <Route path="/signin" element={<SignIn />} />
-      </Routes>
-    </Router>
-        {/* <Registrationform></Registrationform> */}
-        
-
+    <Router>
+      <div className="App">
+        <div className="App-logo">
+          <Routes>
+            <Route path="/eduParent" element={<EduParentDashboard />} />
+            {/* Fallback route to handle undefined paths */}
+            <Route path="*" element={<SignIn />} />
+          </Routes>
         </div>
-    </div>
-
-  )
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
