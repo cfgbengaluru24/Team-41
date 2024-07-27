e83f3c44d6ccc0ac94c3b7d059898fe3db95cf3c
IkanshGoyal <ikanshgoyal@gmail.com>
2024-07-27 18:19:56 +0530

    uri

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,8 @@
-require('dotenv').config();
-require('express-async-errors');
-
 const express = require('express');
 const app = express();
 require('dotenv').config();
 const connectDB = require('./db/connect');
 
-//middlewares
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const rateLimiter = require('express-rate-limit');
-const errorHandlerMiddleware = require('./middleware/error-handler');
-const notFoundMiddleware = require('./middleware/not-found');
-
-const cloudinary = require('cloudinary').v2;
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,  
-});
-
-//routes
-
-
-// using middlewares
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-}));
-
-app.use(helmet());
-app.use(mongoSanitize());
-app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.json());
 
 app.get('/',async(req,res)=>{
@@ -42,8 +11,7 @@ app.get('/',async(req,res)=>{
 
 
 const port = process.env.PORT || 5000;
-console.log(process.env.MONGO_URI);
-console.log(process.env.PORT);
+
 const start = async()=>{
     try {
         await connectDB(process.env.MONGO_URI);
